770208d703370b36603fbab67dfc524c3e7ef910
bandori <76596772+bandori@users.noreply.github.com>
2020-12-28 15:23:00 +0900

    update

--- quiz.js
+++ quiz.js
@@ -12,29 +12,66 @@ var quizNum = 1;
 var randomQuiz;
 var answers = [];
 var currentScore = 0;
+var gameMode = "";
 
 
 $(document).ready(function(){
+  $("#mainGame").css("display","none");
+  gameMode="";
+});
+
+function gameModeSelect(type){
+  switch(type){
+    case 1:
+      gameMode="illust";
+      break;
+    case 2:
+      gameMode="song";
+      break;
+    case 99:
+      gameMode="all";
+      break;
+  }
+  $("#main").css("display","none");
+  $("#mainGame").css("display","block");
+
+  gameReady();
+}
+
+function gameReady(){
   var ajaxRequest = $.ajax({
     url: 'https://bandori.github.io/bandori/json/question.json',
     dataType: 'json',
     success: function(data) {
+      var _num=0;
       $.each(data, function(index){
+        if(gameMode=="all"){
+          quiz[index] = new Question(data[index].type, data[index].quiz, data[index].quizImage, data[index].answer1, data[index].answer2, data[index].answer3, data[index].answer4)
+          //alert(quiz[index].quiz + " " +quiz[index].quizImage + " " + quiz[index].answer1 + " " + quiz[index].answer2
+          //+ " " + quiz[index].answer3 + " " + quiz[index].answer4);
+        }
+        else{
+          if(data[index].type == gameMode){
+            quiz[_num] = new Question(data[index].type, data[index].quiz, data[index].quizImage, data[index].answer1, data[index].answer2, data[index].answer3, data[index].answer4)
+            //alert(quiz[_num].quiz + " " +quiz[_num].quizImage + " " + quiz[_num].answer1 + " " + quiz[_num].answer2
+            //+ " " + quiz[_num].answer3 + " " + quiz[_num].answer4);
+            _num++;
 
-        quiz[index] = new Question(data[index].type, data[index].quiz, data[index].quizImage, data[index].answer1, data[index].answer2, data[index].answer3, data[index].answer4)
-      });
+          }
+        }
+      })
+
     }
   })
 
   $.when(ajaxRequest).done(function(){
     btnProvideQuestion();
-  });
-});
 
-document.addEventListener("DOMContentLoaded", function(event) { 
-});
+  })
 
-function Question(quizType, quiz,quizImage,rightAnswer,wrongAnswer1,wrongAnswer2, wrongAnswer3) {
+}
+
+function Question(quizType,quiz,quizImage,rightAnswer,wrongAnswer1,wrongAnswer2, wrongAnswer3) {
   this.quizType = quizType;  
   this.quiz = quiz;
   this.quizImage = quizImage;
@@ -57,38 +94,36 @@ function btnProvideQuestion() {
   shuffle(answers);
 
   var mainColor = "#EC4E4E";
-
-  document.getElementById("quiz").innerHTML= randomQuiz.quiz;
-
+  var _quizName = document.getElementById("quiz");
   var _quizImage = document.getElementById("quizImage");
+  var _answer1 = document.getElementById("A");
+  var _answer2 = document.getElementById("B");
+  var _answer3 = document.getElementById("C");
+  var _answer4 = document.getElementById("D");
+
+  _quizName.innerHTML= randomQuiz.quiz;
+
   if(randomQuiz.quizImage == ""){
     _quizImage.style.display="none";
   }
   else{
-    var _img = cropImage(_quizImage, {
-      x:_quizImage.width/4,
-      y:_quizImage.height/4,
-      width:_quizImage.width/2,
-      height:_quizImage.height/2,
-    })
-    _img.style.display="block";
-    _img.style.margin="0 auto";
-    _img.src = randomQuiz.quizImage;
+    _quizImage.style.display="block";
+    _quizImage.style.margin="0 auto";
+    _quizImage.src = randomQuiz.quizImage;
   }
+  _answer1.value= answers[0];
+  _answer1.innerHTML= answers[0];
+  _answer2.value= answers[1];
+  _answer2.innerHTML= answers[1];
+  _answer3.value= answers[2];
+  _answer3.innerHTML= answers[2];
+  _answer4.value= answers[3];
+  _answer4.innerHTML= answers[3];
 
-  document.getElementById("A").value= answers[0];
-  document.getElementById("A").innerHTML= answers[0];
-  document.getElementById("B").value= answers[1];
-  document.getElementById("B").innerHTML= answers[1];
-  document.getElementById("C").value= answers[2];
-  document.getElementById("C").innerHTML= answers[2];
-  document.getElementById("D").value= answers[3];
-  document.getElementById("D").innerHTML= answers[3];
-
-  document.getElementById("A").style.backgroundColor = mainColor;
-  document.getElementById("B").style.backgroundColor = mainColor;
-  document.getElementById("C").style.backgroundColor = mainColor;
-  document.getElementById("D").style.backgroundColor = mainColor;
+  _answer1.disabled=false;
+  _answer2.disabled=false;
+  _answer4.disabled=false;
+  _answer4.disabled=false;
 }
 
 function answerA_clicked() {
@@ -124,10 +159,6 @@ function adjustScore(isCorrect) {
   document.getElementById("score").innerHTML = "점수: "+currentScore;
 }
 
-function btnDisable(btn){
-  btn.style.backgroundColor='gray';
-}
-
 function checkAnswer(answer, btn) {  
   if (answer == randomQuiz.rightAnswer) {
     adjustScore(true);
@@ -141,6 +172,10 @@ function checkAnswer(answer, btn) {
   }	  
 }
 
+function btnDisable(btn){
+  btn.disabled=true;
+}
+
 const notification = document.getElementById('notification-container')
 // Show notification
 const showNotification = () => {
@@ -150,13 +185,3 @@ const showNotification = () => {
   }, 2000)
 }
 
-function cropImage(image, croppingCoords) {
-  var cc = croppingCoords;
-  var workCan = document.createElement("canvas"); // create a canvas
-  workCan.width = Math.floor(cc.width);  // set the canvas resolution to the cropped image size
-  workCan.height = Math.floor(cc.height);
-  var ctx = workCan.getContext("2d");    // get a 2D rendering interface
-  ctx.drawImage(image, -Math.floor(cc.x), -Math.floor(cc.y)); // draw the image offset to place it correctly on the cropped region
-  image.src = workCan.toDataURL();       // set the image source to the canvas as a data URL
-  return image;
-}
