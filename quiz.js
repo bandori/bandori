6c0974c28749e8c1d5870341567b577f529172b8
이민영 <minyoung.lee@oceandrive.studio>
2021-09-18 23:57:18 +0900

    Update

--- quiz.js
+++ quiz.js
@@ -83,6 +83,7 @@ function gameModeSelect(type){
 function customQuiz(){
   $("#selectQuiz").css("display","none");
   $("#customQuiz").css("display","block");
+  $("#footer").css("display","none");
 }
 
 function customQuizSelect(type, btn){
